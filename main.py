1277bfd62bd31fa883adf9031fab3a9bc2025fd9
anujamerwade <anujamerwade97@gmail.com>
2024-07-08 11:31:35 -0400

    add images for sky conditions

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import plotly.express as px
 from backend import get_data
 
 st.title("Weather forecast for the next few days")
-place = st.text_input("Place: ")
+place = st.text_input("Place: ", "Mumbai")
 days = st.slider("Forecast days:", min_value=1, max_value=5, help="Select the number of days to forecast for")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-data = get_data(place, days, option)
-# add graph
-dates = ["2022-07-05", "2022-07-06", "2022-07-07"]
-temps = [10, 19, 20]
+filtered_data = get_data(place, days)
 
-figure = px.line(x=dates, y=temps, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+# add temp graph
+if option == "Temperature":
+    temps = [dict["main"]["temp"] for dict in filtered_data]
+    dates = [dict["dt_txt"] for dict in filtered_data]
+    figure = px.line(x=dates, y=temps, labels={"x": "Date", "y": "Temperature (C)"})
+    st.plotly_chart(figure)
+
+images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+          "Rain": "images/rain.png", "Snow": "images/snow.png"}
+if option == "Sky":
+
+    skies = [dict["weather"][0]["main"] for dict in filtered_data]
+    image_paths = [images[condition] for condition in skies]
+    st.image(image_paths, width=115,)
